87a61f224051fe40909a9e7f718aa0adc0ddc1bb
Marco Barros <marco.aurelio.de.barros@hotmail.com>
2021-10-21 10:11:52 -0300

    Projeto iniciado, mostrando a mensagem Ola mundo!

---
+++ testeGitRepository/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testeGitRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
